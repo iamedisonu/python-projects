4ed361b89250ff9634a1b4db98bf26bb0f699fe1
iamedisonu <iamedisonu250@gmail.com>
2024-07-11 09:58:08 +0200

    fibo and fact

---
+++ day13.py
@@ -0,0 +1,17 @@
+#fibonacci
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-2) + fibo(n-1)
+
+terms=int(input("enter a number of terms for fibonacci series: "))
+for n in range (0,terms):
+    print(fibo(n))
+    
+
+
+        
